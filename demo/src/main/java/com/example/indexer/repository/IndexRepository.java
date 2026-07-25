b7008dbe809c8a8d7d6f70f4529b01811a698e7e
blue9207081 <blue920708@becuai.com>
2026-07-25 14:06:16 +0900

    Long

--- demo/src/main/java/com/example/indexer/repository/IndexRepository.java
+++ demo/src/main/java/com/example/indexer/repository/IndexRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 import com.example.indexer.dto.document.NewsDocument;
 
-public interface  IndexRepository extends ElasticsearchRepository<NewsDocument, String> {
+public interface  IndexRepository extends ElasticsearchRepository<NewsDocument, Long> {
 }
